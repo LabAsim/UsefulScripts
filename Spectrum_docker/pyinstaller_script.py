f13add4b367e6eebcc2f5837c89cf0bbe5cdcfab
LabAsim <lampgit@pm.me>
2023-11-16 21:04:12 +0200

    update pyinstaller script

--- Spectrum_docker/pyinstaller_script.py
+++ Spectrum_docker/pyinstaller_script.py
@@ -1,6 +1,9 @@
+import os
+
 import PyInstaller.__main__
 
 name = 'main.py'
+dir_path = os.path.dirname(__file__)
 
 PyInstaller.__main__.run([
     f'{name}',
@@ -10,3 +13,9 @@ PyInstaller.__main__.run([
     '--noconsole',
     '-y'
 ])
+
+try:
+    os.remove(os.path.join(dir_path + '\\dist', 'spectrum_docker.exe'))
+except:
+    pass
+os.rename(os.path.join(dir_path + '\\dist', 'main.exe'), os.path.join(dir_path + '\\dist', 'spectrum_docker.exe'))
