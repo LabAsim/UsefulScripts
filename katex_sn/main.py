0ba06d77184383c298a857f447f62a0699d30142
LabAsim <lampgit@pm.me>
2024-02-17 01:18:56 +0200

    add headers for tex, add lines

--- katex_sn/main.py
+++ katex_sn/main.py
@@ -11,8 +11,29 @@ logger = logging.getLogger()
 def main() -> None:
     args = parser.parse_arguments()
     target_file = args.file
-    pattern = "\$\$"
-    repl_pattern = "$"
+    headers = r"""
+---
+header-includes:
+  - \usepackage{enumitem}
+  - \setlistdepth{20}
+  - \renewlist{itemize}{itemize}{20}
+  - \renewlist{enumerate}{enumerate}{20}
+  - \setlist[itemize]{label=$\cdot$}
+  - \setlist[itemize,1]{label=\textbullet}
+  - \setlist[itemize,2]{label=--}
+  - \setlist[itemize,3]{label=*}
+output:
+  rmarkdown::pdf_document:
+      keep_tex: yes
+---
+\newcommand*\boldborderline[1]{\par\noindent\raisebox{.8ex}{\makebox[\linewidth]{\hrulefill\hspace{1ex}\raisebox{-.8ex}{\textbf{#1}}\hspace{1ex}\hrulefill}}}
+            """
+    # See the command here: https://tex.stackexchange.com/a/15122
+    with open(target_file, 'r+', encoding="utf-8") as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(headers.strip('\r\n') + '\n' + content)
+
     new_content = ""
     with open(target_file, "r+", encoding="utf-8") as file:
         # re.sub needs bytes
@@ -52,6 +73,14 @@ def main() -> None:
         pattern = re.compile(pattern=r"\\hdashline")
         # decode the bytes to str (this way we avoid b"" in the file)
         new_content = re.sub(pattern=pattern, repl="", string=new_content.decode(encoding="utf-8"))
+
+        # Replace `---`
+        pattern = re.compile(pattern=r"----")
+        new_content = re.sub(
+            pattern=pattern,
+            repl=r"\\noindent\\hrulefill",  # r"\\noindent\\rule{\\textwidth}{0.6pt}",
+            string=new_content
+        )
 
     with open(target_file, "w", encoding="utf-8") as file:
         file.write(new_content)
